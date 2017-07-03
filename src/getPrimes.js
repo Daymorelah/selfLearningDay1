785647337baaf7f72d630327c77dd818ed481aba
Daymorelah <demola.hussainin@gmail.com>
2017-07-03 13:40:58 +0100

    Add comments, mke code clearer and more readable

--- src/getPrimes.js
+++ src/getPrimes.js
@@ -1,44 +1,60 @@
+
 /*This script file gets the prime numbers from 0 to n
-where n is d input given to the function.*/
+where n is d input given to the function. It uses ES6 to write the Javascript code.*/
 
 'use strict' ;
 
-
-
+//function that declares and defines function getPrimes.
 function getPrimes(n){
-	let result = [];
+	let result = []; //Declare the output array for the function
+
+//check for invalid inputs
 	if (n < 0){
 		return 'Enter a number greater than 2';
 	}// end of if statement
+
 	else if (n == 0){
 		return 'Enter a number greater than 2';
 	}// end of else if statement
+
 	else if (n == 1){
 		return 'Enter a number greater than 2';
 	}//end of else if statement
+
 	else if (typeof n != 'number'){
 		return 'You can only find prime numbers of an Integer';
 	}//end of else if statement
+
+//check for valid inputs
 	else if (n == 2){
 		result.push(n);
 		return result;
 	}//end of else if statement
+	
 	else{
-		for(let i=2; i<n; i++){
+		
+		for(let i=2; i<=n; i++){
 			let isPrime = true;
+		  
 		  for(let j = 2; j <= (Math.sqrt(i)); j++){
+		     
 		     if(i%j==0){
 		     	isPrime = false;
-		     } //end of if statement
-	}//end of inner for loop
-	if(isPrime==true){
-	result.push(i);
-}//end of if statement
-}//end of outter for loop
-	return result;
-	}//end of outter else statement
-}// end of unction
+		     		} //end of if statement
+		
+			}//end of inner for loop
+	
+			if(isPrime==true){
+				result.push(i);
+					}//end of if statement
 
-let result = getPrimes(12);
-console.log('When input is 12, output is : '+result);
-//export default getPrimes
+		}//end of outter for loop
+	
+	return result; //return an array of prime nubmers from 0 to n.
+	
+	}//end of else statement
+
+}// end of function getPrimes
+
+export default getPrimes
+
--- test/test.js
+++ test/test.js
@@ -4,46 +4,57 @@
 'use strict';
 
 const assert = require('chai').assert;
-import getPreimes from '../src/getPrimes.js'
+import getPrimes from '../src/getPrimes.js'
 
-describe('Funcntion getPreimes',()=>{
+describe('Funcntion getPrimes',()=>{
 
 	describe('Test for invalid inputs',()=>{
-		it('Return Enter a number greater than 2 if input is negative',()=>{
+		it('Return, Enter a number greater than 2, if input is negative',()=>{
 			let result = getPrimes(-3);
 			assert.equal(result,'Enter a number greater than 2');
 		});//end of it block
-		it('Return Enter a number greater than 2 if input is zero',()=>{
+		it('Return, Enter a number greater than 2, if input is zero',()=>{
 			let result = getPrimes(0);
 			assert.equal(result,'Enter a number greater than 2');
 		});//end of it block
-		it('Return Enter a number greater than 2 if input is eual to 1',()=>{
+		it('Return, Enter a number greater than 2, if input is eual to 1',()=>{
 			let result = getPrimes(1);
 			assert.equal(result,'Enter a number greater than 2');
 		});//end of it block
-		it('Return You can only find prime numbers of an Integer if input is a string',()=>{
+		it('Return, You can only find prime numbers of an Integer, if input is a string',()=>{
 			let result = getPrimes('Hello');
 			assert.equal(result,'You can only find prime numbers of an Integer');
 		});//end of it block
-		it('Return You can only find prime numbers of an Integer if input is a string',()=>{
+		it('Return, You can only find prime numbers of an Integer, if input is a special character',()=>{
+			let result = getPrimes('!£$@~!£$%^');
+			assert.equal(result,'You can only find prime numbers of an Integer');
+		});//end of it block
+		it('Return, You can only find prime numbers of an Integer, if input is a string',()=>{
 			let result = getPrimes('45');
 			assert.equal(result,'You can only find prime numbers of an Integer');
 		});//end of it block
 	});//end of first test suite
 
 	describe('Test for valid inputs',()=>{
-		it('Return [2, 3, 5, 7, 11,] if input is 12',()=>{
+		it('Return, [2, 3, 5, 7, 11,] ,if input is 12',()=>{
 			let result = getPrimes(12);
-			assert.equal(result,[2, 3, 5, 7, 11,]);
+			assert.deepEqual(result, [ 2, 3, 5, 7, 11 ]);
 		});//end of it block
-		it('Return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37] if input is 40',()=>{
+		it('Return, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37], if input is 40',()=>{
 			let result = getPrimes(40);
-			assert.equal(result,[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]);
+			assert.deepEqual(result,[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]);
 		});//end of it block
 		it('Return [2] if input is 2',()=>{
 			let result = getPrimes(2);
-			assert.equal(result,[2]);
+			assert.deepEqual(result,[2]);
 		});//end of it block
-		
+		it('Return, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,], if input is 65',()=>{
+			let result = getPrimes(65);
+			assert.deepEqual(result,[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,]);
+		});//end of it block
+		it('Return, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107], if input is 107',()=>{
+			let result = getPrimes(107);
+			assert.deepEqual(result,[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107]);
+		});//end of it block
 	});//end of first test suite
 });//end of main test suite
